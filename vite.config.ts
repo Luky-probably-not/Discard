9a2756d315bc1d3302b50846dbd95da78c3af30f
Cy4n1 <156065290+Cassian-J@users.noreply.github.com>
2026-01-26 21:50:55 +0100

    init the components part

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,11 @@ export default defineConfig({
       '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
       '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
       '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
-      '@Icons': fileURLToPath(new URL('./public/Icons', import.meta.url)),
+      '@icons': fileURLToPath(new URL('./public/Icons', import.meta.url)),
+      '@channels': fileURLToPath(new URL('./src/components/channels', import.meta.url)),
+      '@users': fileURLToPath(new URL('./src/components/users', import.meta.url)),
+      '@messages': fileURLToPath(new URL('./src/components/messages', import.meta.url)),
+      '@logins': fileURLToPath(new URL('./src/components/logins', import.meta.url)),
     },
   },
 })
